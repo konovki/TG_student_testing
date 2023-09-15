3f780c690c6f6b8b34f88d127aa545bbc1b20058
Kirill Konov <konov.k.i@gmail.com>
2023-09-15 08:16:44 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,4 @@
+Tinkoff_token = 't.kt1wfASMY0v2ufIGIVHCWlJ6tYu14bwu9sdn_WgsXK-Dt135-YwtKchpG9dGiTMggyw-cvX2b_xyu70FNFWDaA'
+Telegram_token = '5753114191:AAHOmNtuodpqUqmNaAnftu3NGTh84uM7Mb4'
+trade_list_path = 'by_list'
+My_tg = 933204772
