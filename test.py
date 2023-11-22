afbf0e3e7696eeff4292b0c312e80809dd714277
Kirill Konov <konov.k.i@gmail.com>
2023-11-22 15:47:26 +0300

    2

--- test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-pd.set_option('display.max_rows', None)
-df = pd.read_csv('./df_2023_09_27_11_38_49_465232.csv')
-print(df)
-for name in np.unique(np.array(df['name'])):
-    print('student', name)
-    print(df[df['name']==name])
-#Булдакова4
